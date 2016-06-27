765c2f85ceb9dcf281a5a77bb8b9a84fb0604824
sdavrieux <sdavrieux@gmail.com>
2016-06-27 11:49:47 +0200

    [BACKEND-2] Corretta la selectFrom_LIVELLO_By_idlivello, ora dato che ritornerà sicuramente un solo livello con i propri dati evita di creare un array di livelli.

--- desertodeibarbari/management/management_livello.php
+++ desertodeibarbari/management/management_livello.php
@@ -18,25 +18,25 @@ function selectAllFrom_LIVELLO($connection){
 	}
 	return $fields;
 }
+
 function selectFrom_LIVELLO_By_idlivello($connection,$value){
-	 $query="SELECT idlivello, numero, nome, json FROM livello WHERE idlivello=?";
+  $query="SELECT idlivello, numero, nome, json FROM livello WHERE idlivello=?";
  	$fields = [];
 	if ($stmt = $connection->prepare($query)) {
 		$stmt->bind_param( "s",$value);
 		$stmt->execute();
 		$stmt->bind_result($idlivello, $numero, $nome, $json);
-		$i = 0;
 		while ($stmt->fetch()) {
-			$fields[$i]["idlivello"] = $idlivello;
-			$fields[$i]["numero"] = $numero;
-			$fields[$i]["nome"] = $nome;
-			$fields[$i]["json"] = $json;
-			$i = $i + 1;
+			$fields["idlivello"] = $idlivello;
+			$fields["numero"] = $numero;
+      $fields["nome"] = $nome;
+      $fields["json"] = $json;
 		}
 		$stmt->close();
 	}
 	return $fields;
 }
+
 function selectFrom_LIVELLO_By_numero($connection,$value){
 	 $query="SELECT idlivello, numero, nome, json FROM livello WHERE numero=?";
  	$fields = [];
