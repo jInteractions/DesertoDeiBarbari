43c7bd1ba28162ee44e1289c6577dae65ad89b26
ftrombi <francesco.trombi@outlook.com>
2016-07-01 10:06:38 +0200

    Creazione file livello4.js.

---
+++ missile_command/assets/js/livello4.js
@@ -0,0 +1,21 @@
+function Livello4 ( callbackFineLivello ) {
+  CoreLevel.call( this, callbackFineLivello );
+}
+
+Livello4.prototype = Object.create( CoreLevel.prototype );
+Livello4.prototype.constructor = Livello2;
+
+// interfaccia test - codice utente
+
+
+
+// TAB 1
+
+
+// test
+
+
+// TAB 2
+
+
+// test
