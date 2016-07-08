4ed19a5ee2ce1ea8d49504257449752b2fea5ec3
Andrea <andrea.segalini@studio.unibo.it>
2016-07-08 17:31:02 +0200

    Tarato il livello 5.

--- missile_command/assets/js/livello5.js
+++ missile_command/assets/js/livello5.js
@@ -41,18 +41,18 @@ function Livello5 ( callbackFineLivello ) {
 Livello5.prototype = Object.create( CoreLevel.prototype );
 Livello5.prototype.constructor = Livello5;
 
-Livello5.prototype.inizializzaArmiNemiche = function ( ) {
+Livello5.prototype.inizializzaArmiNemiche = function () {
   var areaPertenza = this.coreGame.canvas.width;
-  var ritardoMassimo = 400;
-  var xRand;
-  var velRand;
+  var ritardoMassimo = 500 * (this.numeroOndata * 0.05);
+  var velMin = 1.3 + this.numeroOndata * 0.05;
+  var velMax = 1.4 + this.numeroOndata * 0.05;
+  var numeroMissili = 18 + Math.floor( this.numeroOndata );
   var ritardoRand;
   var bersagli = this.coreGame.bersagliAttaccabili();
-  var numeroMissili = 15;
   
   for( var i = 0; i < numeroMissili ; i++ ) {
-    xRand = rand( 0, areaPertenza );
-    velRand = rand( 1, 1.5 );
+    var xRand = rand( 0, areaPertenza );
+    var velRand = rand( velMin, velMax );
     ritardoRand = rand( 0, ritardoMassimo );
     this.coreGame.missiliNemici.push( new MissileNemico( {
       coloreTestata: 'yellow',
